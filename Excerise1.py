fb059be7a0ace42227e85fa9956933d9e8e8824e
IGS <IGS@IGS-DELL-2370.igsc.in>
2026-02-26 18:14:31 +0530

    Excercise1 commit

---
+++ Excerise1.py
@@ -0,0 +1,21 @@
+class Students:
+    Name:None
+    Subject1:None
+    Subject2:None
+    Subject3:None
+    Subject4:None
+    Subject5:None
+
+Student_1=Students()
+Name="saru"
+Subject1=12
+Subject2=10
+Subject3=5
+Subject4=9
+Subject5=50
+Subject6=Subject1+Subject2+Subject3+Subject4+Subject5
+
+def studentss(self):
+    print(self.Name + "total marks=" + self.Subject6)
+
+Student_1.studentss()
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 class Human:
-    Name: None  #----state
+    Name: None  #----state(variables)
     Age: None    #----state 
     Weight: None  #----state
     blood_group=None  #----state
 
-    def sleep(self):  #behaviour
+    def sleep(self):  #behaviour(function)
         print(self.Name+"is sleeping")
 
 person=Human()
@@ -17,3 +17,18 @@ print(person.Name)
 print(person.Age)
 person.sleep()
 person.blood_group()
+
+person1=Human
+
+# PASS --EMPTY CLASS OR EMPTY METHOD - TO FULLFILL WITHOUT ANTY ERROR WE USE PASS
+# class Human:
+#     # pass
+# Name: None  #----state(variables)
+# Age: None    #----state 
+# Weight: None  #----state
+# blood_group=None  #----state
+
+# person=Human()
+# Name="sarumathi_vasu"
+# print(Name)
+# Got an error as Expected indented block - here we command pass so we need to declare variable or method with in the class
