4745f3c87c00d129060afbf188df9d872cfdaea1
IGS <IGS@IGS-DELL-2370.igsc.in>
2026-03-04 10:48:07 +0530

    inheritance  added

--- Learning.py
+++ Learning.py
@@ -40,20 +40,65 @@
 # static vs dynamic => why used to save memory so we used
 # if we declare static then it automatically takes
 # in static we cannot able to access the self in 51 line if we use self it got an error 
-class person:
-    s_time=8
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-        self.s_time=10
-    @staticmethod
-    def sleep():
-        print("the person is slepping",person_1.s_time,"hours")
+# class person:
+#     s_time=8
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#         self.s_time=10
+#     @staticmethod
+#     def sleep():
+#         print("the person is slepping",person_1.s_time,"hours")
 
-print(person.s_time)
-person_1=person(name="saru",age=23)
-print(person_1.s_time)
-person.sleep()
+# print(person.s_time)
+# person_1=person(name="saru",age=23)
+# print(person_1.s_time)
+# person.sleep()
 
 # therfore: staticmethod : class level variables
 # classmethod(dynamic) : instance (object) level variables
+
+# inheritance -- where one class can acquire the properties and methods of another class
+# parent class -> common properties and methods also called as super class or base class
+# child class -> inherties the parent class - also called as sub class or dervied class
+# class grandparent:
+#     def __init__(self):
+#         print("swimming")
+
+# class parent:
+
+#     def sing (self):
+#         print("singing")
+#     def __init__(self):
+#         self.networth_of_parent= 50000000
+
+# class child(parent):
+#     def dancing(self):
+#         print("dancing")
+
+# child1=child()
+# child1.sing()
+# print(child1.networth_of_parent)
+
+# single level - class parent - class child
+# multi-level - class grand parent , class parent, class child
+# multiple-level - class grand parent , class parent(no relation with grandparent), class child(relation with both parent and child)
+# hierarchical - single parent - multiple child - class grandparent - childs are both class parent and class child.
+# hybrid - combinations of above four.
+
+# super keyword
+class parent:
+    def drive(self, speed):
+        print("parent is driving at the spped of", speed)
+
+class child(parent):
+    def drive(self, speed):
+        super().drive(speed)
+        # self.drive
+        print("child is driving at ", speed-30)
+        # return super().drive()
+child1=child()
+
+# child2=parent()
+# child2.drive()
+child1.drive(500)
